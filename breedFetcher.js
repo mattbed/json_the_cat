787a99af74cfe6ff58f4b389592807c3d79da42a
Matthew <m.bednarski@gmail.com>
2022-01-20 22:02:55 +0000

    Refactored and added mocha/chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
 const request = require('request');
-const input = process.argv.slice(2);
-const url = "https://api.thecatapi.comblap/v1/breeds/search?q=" + input[0];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('Error! ', error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length !== 0) {
-    console.log(data[0].description);
-    return;
-  }
-  console.log("There was no entry for this breed.");
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length !== 0) {
+      callback(null, data[0].description);
+      return;
+    }
+    callback("Breed not found.", null);
+  });
+};
+
+module.exports = { fetchBreedDescription };
